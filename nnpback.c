272ace24da00b595fa35ca639d48dd4f2ac531b3
Cheng Wang2 <nle5255@N-1328.office.hd>
2018-10-02 11:59:00 +0200

    avx1-i32x8

--- nnpback.c
+++ nnpback.c
@@ -219,6 +219,9 @@ void handle_backend_event(char* evstr) {
          page = (void*)vgg11_page;
       }
 
+      for (i = 0; i < total_page - 1; ++i)
+      	*(float*)((unsigned long)page + i * PAGE_SIZE) = 12.35 + i;
+
       grant_ref = (grant_ref_t*)malloc(sizeof(grant_ref_t) * total_page);
 
       gettimeofday(&start, 0);
